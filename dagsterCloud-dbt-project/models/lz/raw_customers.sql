8c8d6363098141917d4861271b1e7ed735c0e4a4
Tushar Patil <tpatil@phdata.io>
2026-03-04 16:27:58 +0530

    chore: copy demo_kiewit_dbt into dagsterCloud-dbt-project

---
+++ dagsterCloud-dbt-project/models/lz/raw_customers.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized = 'table',
+    database     = 'DAGSTER_DBT_KIEWIT_DB',
+    schema       = 'lz',
+    tags         = ['lz', 'bronze']
+  )
+}}
+
+
+select
+    id                  as customer_id,
+    name                as full_name,
+    CONVERT_TIMEZONE('America/Los_Angeles', 'Asia/Kolkata', current_timestamp()) as _loaded_at,
+    'customer.csv' as _source_file
+from {{ ref('customer') }}
+
+

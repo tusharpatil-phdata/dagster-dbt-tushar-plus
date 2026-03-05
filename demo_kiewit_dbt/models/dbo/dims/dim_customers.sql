02fee54556bc721041119f9c369d93e8272d879f
Tushar Patil <tpatil@phdata.io>
2026-03-05 14:41:25 +0530

    chore: update dbt configs to DAGSTER_DBT_KIEWIT_DB_PLUS

--- demo_kiewit_dbt/models/dbo/dims/dim_customers.sql
+++ demo_kiewit_dbt/models/dbo/dims/dim_customers.sql
@@ -1,17 +1,19 @@
+
+-- Gold layer: customer dimension in DBO schema
+
+
 {{
   config(
     materialized = 'table',
-    database     = 'DAGSTER_DBT_KIEWIT_DB',
-    schema       = 'dbo',
+    database     = 'DAGSTER_DBT_KIEWIT_DB_PLUS',
+    schema       = 'DBO',
     tags         = ['dbo', 'gold']
   )
 }}
 
-
 with customers as (
     select * from {{ ref('stg_customers') }}
 )
-
 
 select
     customer_id,
--- demo_kiewit_dbt/models/lz/raw_customers.sql
+++ demo_kiewit_dbt/models/lz/raw_customers.sql
@@ -1,18 +1,19 @@
+
+--Bronze layer: load raw customers from seed into LZ schema
+
+
 {{
   config(
     materialized = 'table',
-    database     = 'DAGSTER_DBT_KIEWIT_DB',
-    schema       = 'lz',
+    database     = 'DAGSTER_DBT_KIEWIT_DB_PLUS',
+    schema       = 'LZ',
     tags         = ['lz', 'bronze']
   )
 }}
 
-
 select
-    id                  as customer_id,
-    name                as full_name,
+    id   as customer_id,
+    name as full_name,
     CONVERT_TIMEZONE('America/Los_Angeles', 'Asia/Kolkata', current_timestamp()) as _loaded_at,
     'customer.csv' as _source_file
 from {{ ref('customer') }}
-
-
